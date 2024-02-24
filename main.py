62f27456d3239125c08e6ff0028ee5708ffc5080
ultimateform <heliojmsemedo@gmail.com>
2024-02-24 19:54:53 +0000

    fruther cleanup

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Wagger(discord.Client):
         headers = {
             "User-Agent": user_agent
         }
-        response = requests.get(url, {headers:headers})
+        response = requests.get(url, headers=headers)
         json_response = response.json()
         embed.add_field(name="Culprit Name", value=json_response["name"])
         embed.add_field(name="Culprit Platform", value=json_response["platform"])
@@ -49,7 +49,7 @@ class Wagger(discord.Client):
     async def on_message(self, message: discord.message.Message):
         if message.content == "ping":
             await self.send("Pong")
-        elif len(message.embeds) > 0 and  message.channel.id != self._pun_channel_id:
+        elif len(message.embeds) > 0 and str(message.channel.id) != self._pun_channel_id:
             print(f"received message {message}")
             embed = message.embeds[0]
             source_dict = embed.to_dict()
@@ -60,7 +60,10 @@ class Wagger(discord.Client):
             messageValue = messageKey[0]
             true_message = messageValue.replace("Message:** ", "")
             if any(true_message.startswith(pun) for pun in PUNISHMENTS):
-                await self.send_to_punishments(new_embed, true_message)
+                try:
+                    await self.send_to_punishments(new_embed, true_message)
+                except Exception as e:
+                    print(f"Failed to send punishment log. {e}")
 
 
 
