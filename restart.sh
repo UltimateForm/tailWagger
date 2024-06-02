cb83cc8e5f97b7ec3163da7db69b4a1777fbfdd4
ultimateform <heliojmsemedo@gmail.com>
2024-06-02 19:22:23 +0100

    serverlogs

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 docker stop mordhau-bot-container
 docker rm mordhau-bot-container
 docker build . -t mordhau-bot
-docker run -d -v ./persist/:/bot/persist/ --name mordhau-bot-container mordhau-bot
+docker run -d -v ./persist/:/bot/persist/ -v /home/mhserver/log/server:/bot/serverlogs --name mordhau-bot-container mordhau-bot
